be320c461dcf05dc74e37ce3519928e3e6ddbb6e
Neal Borkert <neal.borkert@gmail.com>
2016-02-13 14:49:56 -0500

    staying in sync

--- getPage.go
+++ getPage.go
@@ -1,9 +1,11 @@
 package getPage
 
 import (
+	"fmt"
 	"io/ioutil"
-	"net/http"
 	"log"
+	"net/http"
+	"time"
 )
 
 func PrintMessage() string {
@@ -14,8 +16,57 @@ func PrintEcho(s string) string {
 	return s
 }
 
+func GetHTTPCode(u string, num int, cresults chan<- int) {
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", u, nil)
+	req.Header.Add("Authorization", "anJrDZQ9M5PYvmGuADqYAxbkdPxUkHsFvoj1EWLj4WFOKHO4TqUFOx8PmtuubuWO")
+
+	t := time.Now()
+	res, err := client.Do(req)
+	t2 := time.Now()
+
+	if err != nil {
+		//log.Fatal(err)
+		log.Println(err)
+	}
+	if err == nil {
+		ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		fmt.Printf("%v,"+res.Status+",%v\n", num, t2.Sub(t))
+	}
+	
+	cresults <- 1
+}
+
+
+func GetHTTPCodeNoChannel(u string, num int) {
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", u, nil)
+	
+	t := time.Now()
+	res, err := client.Do(req)
+	t2 := time.Now()
+
+	if err != nil {
+		//log.Fatal(err)
+		log.Println(err)
+	}
+	if err == nil {
+		ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		fmt.Printf("%v,"+res.Status+",%v\n", num, t2.Sub(t))
+	}	
+}
+
+
 func GetContent(u string) string {
+
 	res, err := http.Get(u)
+
 	if err != nil {
 		log.Fatal(err)
 	}
--- getPage_test.go
+++ getPage_test.go
@@ -2,24 +2,30 @@ package getPage
 
 import "testing"
 
-func TestPrintMessage (t *testing.T) {
+func TestPrintMessage(t *testing.T) {
 	if x := PrintMessage(); x == "" {
 		t.Errorf("Expected text, got empty string")
 	}
 
-
 }
 
-func TestPrintMessageEcho (t *testing.T) {
+func TestPrintMessageEcho(t *testing.T) {
 	if x := PrintEcho("Hello"); x != "Hello" {
 		t.Errorf("Expected Hello, got ", x)
 	}
 
 }
 
-func TestGetContent (t *testing.T) {
+func TestGetContent(t *testing.T) {
 	page := "http://www.example.com"
 	if x := GetContent(page); x == "" {
+		t.Errorf("Did not receive content from ", page)
+	}
+}
+
+func TestGetSecureContent(t *testing.T) {
+	page := "https://example.com:443"
+	if x := GetContent(page); x == "" { 		
 		t.Errorf("Did not receive content from ", page)
 	}
 }

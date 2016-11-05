0e425a14a1590fc5af50473c352f7eb328a4cf2f
Neal Borkert <neal.borkert@gmail.com>
2016-11-05 17:00:37 -0400

    removed unnecessary tests

--- getPage_test.go
+++ getPage_test.go
@@ -2,20 +2,6 @@ package getPage
 
 import "testing"
 
-func TestPrintMessage(t *testing.T) {
-	if x := PrintMessage(); x == "" {
-		t.Errorf("Expected text, got empty string")
-	}
-
-}
-
-func TestPrintMessageEcho(t *testing.T) {
-	if x := PrintEcho("Hello"); x != "Hello" {
-		t.Errorf("Expected Hello, got ", x)
-	}
-
-}
-
 func TestGetContent(t *testing.T) {
 	page := "http://www.example.com"
 	if x := GetContent(page); x == "" {
@@ -25,7 +11,7 @@ func TestGetContent(t *testing.T) {
 
 func TestGetSecureContent(t *testing.T) {
 	page := "https://example.com:443"
-	if x := GetContent(page); x == "" { 		
+	if x := GetContent(page); x == "" {
 		t.Errorf("Did not receive content from ", page)
 	}
 }
